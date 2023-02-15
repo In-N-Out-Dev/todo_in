5c1559461b80eb289d5a7bca3a496dd933a1e1a1
안호진 <asdv94@naver.com>
2023-02-16 00:51:02 +0900

    chore(todo): add recoild dependency

--- .pnp.cjs
+++ .pnp.cjs
@@ -42,6 +42,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["next", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:13.1.6"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:18.2.0"],\
+            ["recoil", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:0.7.6"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=23ec76"]\
           ],\
           "linkType": "SOFT"\
@@ -2689,6 +2690,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["hamt_plus", [\
+        ["npm:1.0.2", {\
+          "packageLocation": "./.yarn/cache/hamt_plus-npm-1.0.2-67a52ee1df-af26ea32db.zip/node_modules/hamt_plus/",\
+          "packageDependencies": [\
+            ["hamt_plus", "npm:1.0.2"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["has", [\
         ["npm:1.0.3", {\
           "packageLocation": "./.yarn/cache/has-npm-1.0.3-b7f00631c1-b9ad53d53b.zip/node_modules/has/",\
@@ -3921,6 +3931,37 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["recoil", [\
+        ["npm:0.7.6", {\
+          "packageLocation": "./.yarn/cache/recoil-npm-0.7.6-6f8de04e3d-2b31dd29a7.zip/node_modules/recoil/",\
+          "packageDependencies": [\
+            ["recoil", "npm:0.7.6"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:0.7.6", {\
+          "packageLocation": "./.yarn/__virtual__/recoil-virtual-2435a77755/0/cache/recoil-npm-0.7.6-6f8de04e3d-2b31dd29a7.zip/node_modules/recoil/",\
+          "packageDependencies": [\
+            ["recoil", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:0.7.6"],\
+            ["@types/react", "npm:18.0.27"],\
+            ["@types/react-dom", "npm:18.0.10"],\
+            ["@types/react-native", null],\
+            ["hamt_plus", "npm:1.0.2"],\
+            ["react", "npm:18.2.0"],\
+            ["react-dom", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:18.2.0"],\
+            ["react-native", null]\
+          ],\
+          "packagePeers": [\
+            "@types/react-dom",\
+            "@types/react-native",\
+            "@types/react",\
+            "react-dom",\
+            "react-native",\
+            "react"\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["regenerator-runtime", [\
         ["npm:0.13.11", {\
           "packageLocation": "./.yarn/cache/regenerator-runtime-npm-0.13.11-90bf536060-27481628d2.zip/node_modules/regenerator-runtime/",\
@@ -4419,6 +4460,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["next", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:13.1.6"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:18.2.0"],\
+            ["recoil", "virtual:d2f066d08463f38772127898de8cc2037b38e50d45dac3d1ed58b660b6a61488cdf52bf1a255815f98ac076475d08144b9b9a154aa44b895c1aac90c5379a708#npm:0.7.6"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=23ec76"]\
           ],\
           "linkType": "SOFT"\
