08a8bb78ae1d341609f0d82e4ef366575e5e6099
안호진 <asdv94@naver.com>
2023-02-17 00:38:29 +0900

    refactor(hooks): auth atome을 login 로직에 추가

--- src/hooks/mutations/useLoginMutation.ts
+++ src/hooks/mutations/useLoginMutation.ts
@@ -1,16 +1,28 @@
-import {ROUTES, TOKEN} from '@/constants';
+import {authState} from '@/atom';
+import {LOCALSTORAGE_KEY, ROUTES} from '@/constants';
 import {postLogin} from '@/http';
 import {useMutation} from '@tanstack/react-query';
 import {useRouter} from 'next/router';
+import {useRecoilState} from 'recoil';
 
 export const useLoginMutation = () => {
   const router = useRouter();
+  const [, setAuthState] = useRecoilState(authState);
   return useMutation({
     mutationFn: postLogin,
     onSuccess: data => {
+      console.log(data);
       const token = data.token;
-      window.localStorage.setItem(TOKEN, token);
-      router.replace(ROUTES.home);
+      if (token) {
+        window.localStorage.setItem(LOCALSTORAGE_KEY.token, token);
+        setAuthState({isLogined: true});
+        router.replace(ROUTES.home);
+        return;
+      }
+      alert(data);
+    },
+    onError: err => {
+      console.error('error', err);
     },
   });
 };
