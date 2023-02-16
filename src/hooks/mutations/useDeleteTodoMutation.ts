978a0be3e867a767f3863b6c1608c4c3a8946a84
안호진 <asdv94@naver.com>
2023-02-16 22:24:22 +0900

    refactor(hooks): add maintain state after refresh logic

--- src/hooks/mutations/useDeleteTodoMutation.ts
+++ src/hooks/mutations/useDeleteTodoMutation.ts
@@ -1,14 +1,20 @@
-import {QUERY_KEYS} from '@/constants';
+import {todoDetailState} from '@/atom';
+import {QUERY_KEYS, LOCALSTORAGE_KEY} from '@/constants';
 import {deleteTodo} from '@/http';
+import {TodoType} from '@/types';
 import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {useRecoilState} from 'recoil';
 
 export const useDeleteTodoMutation = (id: string) => {
   const client = useQueryClient();
+  const [, setTodoDetail] = useRecoilState(todoDetailState);
   return useMutation({
-    mutationFn: deleteTodo,
+    mutationFn: () => deleteTodo(id),
     onSuccess: () => {
       client.invalidateQueries([QUERY_KEYS.todos]);
       client.removeQueries([QUERY_KEYS.todos, id]);
+      window.localStorage.removeItem(LOCALSTORAGE_KEY.todoDetail);
+      setTodoDetail({isOpened: false, todo: <TodoType>{}});
     },
   });
 };
--- src/hooks/mutations/useUpdateTodoMutation.ts
+++ src/hooks/mutations/useUpdateTodoMutation.ts
@@ -1,14 +1,19 @@
-import {QUERY_KEYS} from '@/constants';
+import {todoDetailState} from '@/atom';
+import {LOCALSTORAGE_KEY, QUERY_KEYS} from '@/constants';
 import {updateTodo} from '@/http';
 import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {useRecoilState} from 'recoil';
 
 export const useUpdateTodoMutation = (id: string) => {
   const client = useQueryClient();
+  const [{isOpened}, setTodoDetail] = useRecoilState(todoDetailState);
   return useMutation({
-    mutationFn: updateTodo,
-    onSuccess: () => {
+    mutationFn: updateTodo(id),
+    onSuccess: todo => {
       client.invalidateQueries([QUERY_KEYS.todos]);
       client.invalidateQueries([QUERY_KEYS.todos, id]);
+      window.localStorage.setItem(LOCALSTORAGE_KEY.todoDetail, JSON.stringify(todo));
+      setTodoDetail({isOpened, todo});
     },
   });
 };
