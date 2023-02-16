e53bd01a633bb0ac9212f39d5300282ca4737127
안호진 <asdv94@naver.com>
2023-02-16 23:08:10 +0900

    style(hooks): query hook 이름 수정

--- src/hooks/queries/useTodoDetailQuery.ts
+++ src/hooks/queries/useTodoDetailQuery.ts
@@ -2,7 +2,7 @@ import {QUERY_KEYS} from '@/constants';
 import {getTodoById} from '@/http';
 import {useQuery} from '@tanstack/react-query';
 
-export const useTodoItemQuery = (id: string) => {
+export const useTodoDetailQuery = (id: string) => {
   return useQuery({
     queryFn: () => getTodoById(id),
     queryKey: [QUERY_KEYS.todos, id],
